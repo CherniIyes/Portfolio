1122ab3c44f99fb3054017af7211fee51d508060
CherniIyes <cherniiyes@gmail.com>
2024-11-28 13:11:39 +0100

    polishing

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Footer from "@/components/Footer";
 import Clients from "@/components/Clients";
 import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
-import RecentProjects from "@/components/RecentProjects";git 
+import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
