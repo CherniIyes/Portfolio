141d663d2ee1106ea408ba8bfe19d42330d7b7e0
CherniIyes <cherniiyes@gmail.com>
2024-11-30 10:13:27 +0100

    polishing

--- data/index.ts
+++ data/index.ts
@@ -211,13 +211,16 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    link: "https://github.com/CherniIyes",
   },
   {
     id: 2,
     img: "/twit.svg",
+    link: "https://x.com/cherniiy3s",
   },
   {
     id: 3,
     img: "/link.svg",
+    link: "https://www.linkedin.com/in/cherni-iyes-93003b2b9/",
   },
 ];
